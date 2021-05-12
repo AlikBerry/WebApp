af722bf75af3c3b89a4fb3862bce2addd7f9c4bf
AlikBerry <sahibalekberli@gmail.com>
2021-05-12 14:01:37 +0300

    prefinal_updates

--- DocApp/settings.py
+++ DocApp/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'DocApp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'DocApp.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': str(BASE_DIR / 'db.sqlite3'),
     }
 }
 

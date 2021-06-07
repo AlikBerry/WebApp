e1bf676c2890e144ce117647a064c6f8cb228011
AlikBerry <sahibalekberli@gmail.com>
2021-06-08 00:30:00 +0300

    new_updates_models

--- app_2/models.py
+++ app_2/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 from app_1.models import Client
+from datetime import *
 
 # Create your models here.
 
 class Transacions(models.Model):
 
-    create_date = models.DateField(blank=True, null=True, auto_now=False, auto_now_add=False, verbose_name='Дата')
+    create_date = models.DateField(blank=True, null=True, default=datetime.now, auto_now=False, auto_now_add=False, verbose_name='Дата')
     incoming = models.PositiveIntegerField(blank=True, null=True, default=0, verbose_name='Приход')
     expense = models.PositiveIntegerField(blank=True, null=True, default=0, verbose_name='Расход')
     balance = models.IntegerField(blank=True, null=True, default=0, verbose_name='Баланс')
